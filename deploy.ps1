b478e90456b3a04f7df6d0e1f8bc3f33aac997d3
Danil Pavlov <d2funlife@gmail.com>
2018-06-24 21:25:01 +0300

    Change deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = 'Stop';
 docker images
 
 Write-Host Starting deploy
-docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
-
+docker login d2funlife
+$env:DOCKER_USER
+$env:DOCKER_PASS
 docker push d2funlife/blog:latest
