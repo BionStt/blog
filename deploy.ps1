b8cb04cae271e3a968833e3cd0d28614d3ec0ffe
Danil Pavlov <d2funlife@gmail.com>
2018-06-24 20:02:24 +0300

    Change CI scripts

--- deploy.ps1
+++ deploy.ps1
@@ -2,12 +2,7 @@ $ErrorActionPreference = 'Stop';
 
 docker images
 
-if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
-  Write-Host "No version tag detected. Skip publishing."
-  exit 0
-}
-
 Write-Host Starting deploy
 docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
-docker push d2funlife/blog
+docker push d2funlife/blog:latest
