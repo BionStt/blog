ec8d6a8a7f2207a676808a8e8c68ee9493e92332
Danil Pavlov <d2funlife@gmail.com>
2018-06-24 18:55:16 +0300

    Add CI via Appveyor

---
+++ app/deploy.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop';
+
+docker images
+
+if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
+  Write-Host "No version tag detected. Skip publishing."
+  exit 0
+}
+
+Write-Host Starting deploy
+docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
+
+docker push d2funlife/blog:latest
