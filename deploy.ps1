0e6dddba6416a98b844032fdf56610b163547e08
Danil Pavlov <d2funlife@gmail.com>
2018-06-25 22:17:59 +0300

    Change CI scripts

--- deploy.ps1
+++ deploy.ps1
@@ -3,15 +3,10 @@ Write-Host Start deploy $Version
 
 docker images
 
-Write-Host Tag docker image
-
-docker tag d2funlife/blog d2funlife/blog:$Version
-
 Write-Host Docker login
 
 $env:DOCKER_PASS | docker login --username d2funlife --password-stdin
 
 Write-Host Push to docker hub
 
-docker push d2funlife/blog:$Version
-docker push d2funlife/blog:latest
+docker push d2funlife/blog
