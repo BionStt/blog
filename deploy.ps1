eed308302669ee8b206739c4500cf5b0a0f8f548
Danil Pavlov <d2funlife@gmail.com>
2018-06-25 20:42:24 +0300

    Change CI build script

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,15 @@
-$ErrorActionPreference = 'Stop';
+Write-Host Start deploy $env:APPVEYOR_REPO_TAG_NAME
 
 docker images
 
-Write-Host Starting deploy
+Write-Host Tag docker image
+
+docker tag d2funlife/blog:$env:APPVEYOR_REPO_TAG_NAME
+
+Write-Host Docker login
+
 $env:DOCKER_PASS | docker login --username d2funlife --password-stdin
+
+Write-Host Push to docker hub
 
 docker push d2funlife/blog:latest
